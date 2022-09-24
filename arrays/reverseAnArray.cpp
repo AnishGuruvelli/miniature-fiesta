fb1865f336a1131597a59f7a227b4f8102aee879
AnishGuruvelli <guruvellianish@gmail.com>
2022-09-24 09:24:13 +0530

    reverseArray

--- arrays/reverseAnArray.cpp
+++ arrays/reverseAnArray.cpp
@@ -14,6 +14,19 @@ void reverseArray(int arr[], int start, int end) {
         start++;
         end--;
     }
+
+    // or
+    /*
+    if (start >= end)
+    return;
+     
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+     
+    // Recursive Function calling
+    rvereseArray(arr, start + 1, end - 1);
+    */
 }   
 
 void printArray(int arr[], int n) {
@@ -41,3 +54,36 @@ int main() {
     return 0;
 }
 
+/*
+or
+Stack-based Approach: Follow the steps below to solve the problem:
+
+    Initialize a Stack to store the array elements.
+    Traverse the array and push all the array elements into the stack.
+    >Pop the elements from the stack and insert into the array.
+    Finally, print the array.
+
+    Below is the implementation of the above approach:
+*/
+
+/*
+for (int i = 0; i < n; i++) {
+  
+        // Insert arr[i] into the stack
+        push(stack, arr[i]);
+    }
+  
+    // Reverse the array elements
+    for (int i = 0; i < n; i++) {
+  
+        // Update arr[i]
+        arr[i] = pop(stack);
+    }
+  
+    // Print array elements
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
+
+
+*/
