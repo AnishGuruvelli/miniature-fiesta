c2754044e40aa86e70c7a8ca9894f8cc5ce010d0
AnishGuruvelli <guruvellianish@gmail.com>
2022-09-29 13:08:49 +0530

    29/09

---
+++ strings/ validParentheses.cpp
@@ -0,0 +1,46 @@
+class Solution
+{
+public:
+    bool isValid(string a)
+    {
+        stack<char> s;
+        int n = a.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == '(' || a[i] == '{' || a[i] == '[')
+            {
+                s.push(a[i]);
+            }
+            else
+            {
+                if (s.empty())
+                    return false;
+                else if (a[i] == ')')
+                {
+                    if (s.top() == '(')
+                        s.pop();
+                    else
+                        return false;
+                }
+                else if (a[i] == '}')
+                {
+                    if (s.top() == '{')
+                        s.pop();
+                    else
+                        return false;
+                }
+                else if (a[i] == ']')
+                {
+                    if (s.top() == '[')
+                        s.pop();
+                    else
+                        return false;
+                }
+            }
+        }
+        if (s.empty())
+            return true;
+        else
+            return false;
+    }
+};
