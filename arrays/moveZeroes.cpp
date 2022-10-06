45e2f3e72961b0da4bb8ec129273f516c289e8d9
AnishGuruvelli <guruvellianish@gmail.com>
2022-10-06 16:28:32 +0530

    06/10

---
+++ arrays/moveZeroes.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int n = nums.size();
+        int j = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        while (j < nums.size())
+        {
+            nums[j] = 0;
+            j++;
+        }
+    }
+};
+
+// https://www.youtube.com/watch?v=mgzcjQ1x9Mw&list=PLKZaSt2df1gwgL9IH2rH9l9HcXbNx16hx&index=30
+// https://leetcode.com/problems/move-zeroes/
