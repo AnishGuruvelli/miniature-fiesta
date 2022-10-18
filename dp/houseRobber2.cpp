6e54f45c93f33b3b84b953f03bd5b8ed516ed88d
AnishGuruvelli <guruvellianish@gmail.com>
2022-10-18 16:48:50 +0530

    18/10

--- dp/houseRobber2.cpp
+++ dp/houseRobber2.cpp
@@ -4,14 +4,22 @@ public:
     int solve(vector<int> &nums)
     {
         int n = nums.size();
-        vector<int> dp(n + 1);
-        dp[0] = 0;
-        dp[1] = nums[0];
-        for (int i = 2; i <= n; i++)
+        vector<int> dp(n);
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return nums[0];
+        if (n == 2)
+            return max(nums[0], nums[1]);
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++)
         {
-            dp[i] = max(nums[i - 1] + dp[i - 2], dp[i - 1]);
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
         }
-        return dp[n];
+        return dp[n - 1];
     }
     int rob(vector<int> &nums)
     {
