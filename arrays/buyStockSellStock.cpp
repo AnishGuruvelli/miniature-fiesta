04d68f16d679b05f2e86171fcf619806d60a3891
AnishGuruvelli <guruvellianish@gmail.com>
2022-11-26 15:44:06 +0530

    26/11

--- arrays/buyStockSellStock.cpp
+++ arrays/buyStockSellStock.cpp
@@ -1,36 +1,36 @@
-class Solution
-{
-public:
-    int maxProfit(vector<int> &prices)
-    {
-        int n = prices.size();
-        int maxProfit = 0;
-        int buy = prices[0];
-        for (int i = 1; i < n; i++)
-        {
-            if (buy > prices[i])
-            {
-                buy = prices[i];
-            }
-            if (prices[i] - buy > maxProfit)
-            {
-                maxProfit = prices[i] - buy;
-            }
-        }
-        return maxProfit;
-    }
-};
+// class Solution
+// {
+// public:
+//     int maxProfit(vector<int> &prices)
+//     {
+//         int n = prices.size();
+//         int maxProfit = 0;
+//         int buy = prices[0];
+//         for (int i = 1; i < n; i++)
+//         {
+//             if (buy > prices[i])
+//             {
+//                 buy = prices[i];
+//             }
+//             if (prices[i] - buy > maxProfit)
+//             {
+//                 maxProfit = prices[i] - buy;
+//             }
+//         }
+//         return maxProfit;
+//     }
+// };
 
 class Solution
 {
 public:
     int maxProfit(vector<int> &prices)
     {
-        // minB in minimum price to buy and maxP is max profit one can make doing this
+        // minB is minimum price to buy and maxP is max profit one can make doing this
         int n = prices.size();
         int minB = prices[0], maxP = INT_MIN;
 
-        for (int i = 1; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
             minB = min(minB, prices[i]);
             maxP = max(maxP, prices[i] - minB);
