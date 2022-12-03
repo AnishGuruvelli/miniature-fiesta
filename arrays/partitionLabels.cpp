3ed17ad205fddfb493360a2068ef5d0eaa2c8570
AnishGuruvelli <guruvellianish@gmail.com>
2022-12-03 11:04:55 +0530

    3/12

---
+++ arrays/partitionLabels.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    vector<int> partitionLabels(string s)
+    {
+        vector<int> last(26, 0);
+
+        // getting last index of every letter
+        for (int i = 0; i < s.size(); i++)
+        {
+            last[s[i] - 'a'] = i;
+        }
+
+        vector<int> v;
+        
+        // 2 pointers both at the start only, and then l is for calculating the length between
+        // the start and the last element of that letter.
+        int start = 0, l = 0;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            l = max(l, last[s[i] - 'a']);
+
+            if (i == l)
+            {
+                v.push_back(i - start + 1);
+                start = l + 1;
+            }
+        }
+        return v;
+    }
+};
