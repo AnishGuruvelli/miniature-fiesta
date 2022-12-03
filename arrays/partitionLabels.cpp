bb5a8953ab00b383e286fd86ae3849ce35e647aa
AnishGuruvelli <guruvellianish@gmail.com>
2022-12-03 11:05:52 +0530

    3/12

--- arrays/partitionLabels.cpp
+++ arrays/partitionLabels.cpp
@@ -1,3 +1,6 @@
+// very imp question, we have to find out the last index of every char in a string 
+// https://leetcode.com/problems/partition-labels/description/
+
 class Solution
 {
 public:
@@ -12,7 +15,7 @@ public:
         }
 
         vector<int> v;
-        
+
         // 2 pointers both at the start only, and then l is for calculating the length between
         // the start and the last element of that letter.
         int start = 0, l = 0;
