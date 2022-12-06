fde2991ed93d09572166fa4865ab3b5213956cbc
AnishGuruvelli <guruvellianish@gmail.com>
2022-12-06 11:08:37 +0530

    06/12

---
+++ linkedLists/swapNodesInPairs.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    // ListNode* swapPairs(ListNode* head) {
+    //     vector<int> ans;
+    //     while(head){
+    //         ans.push_back(head->val);
+    //         head = head->next;
+    //     }
+    //     int temp = 0;
+    //     for(int i=0;i<ans.size();i+=2){
+    //         temp = ans[i];
+    //         ans[i] = ans[i+1];
+    //         ans[i+1] = temp;
+    //     }
+
+    //     ListNode *dummy = new ListNode(0);
+    //     ListNode *curr = dummy;
+    //     for(auto i: ans){
+
+    //         cout<<i<<endl;
+
+    //         ListNode *node = new ListNode(i);
+    //         curr->next = node;
+    //         curr = curr->next;
+    //     }
+    //     return dummy->next;
+    // }
+
+    ListNode *swapPairs(ListNode *head)
+    {
+        if (!head || !head->next)
+            return head;
+        // If there are less than 2 nodes in the given nodes,
+        // then no need to do anything just return the list as it is.
+
+        ListNode *dummyNode = new ListNode();
+
+        ListNode *prevNode = dummyNode;
+        ListNode *currNode = head;
+
+        while (currNode && currNode->next)
+        {
+            prevNode->next = currNode->next;
+            currNode->next = prevNode->next->next;
+            prevNode->next->next = currNode;
+
+            prevNode = currNode;
+            currNode = currNode->next;
+        }
+
+        return dummyNode->next;
+    }
+};
