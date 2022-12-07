247eeacc3d0460da3f0b5896f36805959f37a03c
AnishGuruvelli <guruvellianish@gmail.com>
2022-12-07 08:38:43 +0530

    07/12

--- trees/rangeSumOfBST.cpp
+++ trees/rangeSumOfBST.cpp
@@ -16,14 +16,13 @@ public:
     {
         if (!root || low > high)
             return 0;
+
         if (root->val < low)
-        {
             return rangeSumBST(root->right, low, high);
-        }
+
         if (root->val > high)
-        {
             return rangeSumBST(root->left, low, high);
-        }
+
         return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
     }
 };
