945ae13ad5f5eb554450ee2c95a9ec9be519a079
AnishGuruvelli <guruvellianish@gmail.com>
2022-12-28 20:19:20 +0530

    28/12

--- arrays/maxBagsWithFullCapacityOfRocks.cpp
+++ arrays/maxBagsWithFullCapacityOfRocks.cpp
@@ -1,38 +1,48 @@
-// class Solution {
+// class Solution
+// {
 // public:
-//     int maximumBags(vector<int>& c, vector<int>& rocks, int b) {
-//         vector<int>v;
-//         for(int i=0;i<rocks.size();i++)     v.push_back(c[i]-rocks[i]);
-//         sort(v.begin(),v.end());
-//         int i=0;
-//         while(b>0 and i<c.size()){
-//             b-=v[i];
+//     int maximumBags(vector<int> &c, vector<int> &rocks, int b)
+//     {
+//         vector<int> v;
+//         for (int i = 0; i < rocks.size(); i++)
+//             v.push_back(c[i] - rocks[i]);
+//         sort(v.begin(), v.end());
+//         int i = 0;
+//         while (b > 0 and i < c.size())
+//         {
+//             b -= v[i];
 //             i++;
 //         }
-//         return b<0 ? i-1 : i;
+//         return b < 0 ? i - 1 : i;
 //     }
 // };
 
-class Solution {
+class Solution
+{
 public:
-    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
-        int cnt = 0;
-        
-        for(int i=0;i<capacity.size();i++) capacity[i] = capacity[i] - rocks[i];
-        
-        sort(begin(capacity),end(capacity));
-        
-        for(auto it : capacity){
-            if(it == 0){ 
-                cnt++;
+    int maximumBags(vector<int> &capacity, vector<int> &rocks, int additionalRocks)
+    {
+        int count = 0;
+
+        for (int i = 0; i < capacity.size(); i++)
+            capacity[i] = capacity[i] - rocks[i];
+
+        sort(begin(capacity), end(capacity));
+
+        for (auto it : capacity)
+        {
+            if (it == 0)
+            {
+                count++;
                 continue;
             }
             additionalRocks -= it;
-            if(additionalRocks >=0) cnt++;
-            else break;
+            if (additionalRocks >= 0)
+                count++;
+            else
+                break;
         }
-        
-        return cnt;
-        
+
+        return count;
     }
 };
