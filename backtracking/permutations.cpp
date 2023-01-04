a7cca8bf723ca0991a4496cb5e4c02eb8e71e9ad
AnishGuruvelli <guruvellianish@gmail.com>
2023-01-04 23:48:48 +0530

    04/01

--- backtracking/permutations.cpp
+++ backtracking/permutations.cpp
@@ -26,32 +26,3 @@ public:
         return ans;
     }
 };
-
-/*
-class Solution
-{
-public:
-    vector<vector<int>> ans;
-
-    void permutations(vector<int> &nums, int n, int i)
-    {
-        if (n == i)
-        {
-            ans.push_back(nums);
-        }
-
-        for (int j = i; j < n; j++)
-        {
-            swap(nums[i], nums[j]);
-            permutations(nums, n, i + 1);
-            swap(nums[i], nums[j]);
-        }
-    }
-
-    vector<vector<int>> permuteUnique(vector<int> &nums)
-    {
-        permutations(nums, nums.size(), 0);
-        return ans;
-    }
-};
-* /
