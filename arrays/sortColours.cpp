084d041a3fb958f4cf1cdd6371ce0dae482fedf5
AnishGuruvelli <guruvellianish@gmail.com>
2023-01-06 12:10:10 +0530

    06/01

--- arrays/sortColours.cpp
+++ arrays/sortColours.cpp
@@ -1,27 +1,39 @@
+// class Solution
+// {
+// public:
+//     void sortColors(vector<int> &nums)
+//     {
+//         // sort(nums.begin(), nums.end());
+
+//         // bubble sort
+//         int i, j;
+//         int n = nums.size();
+
+//         // Last i elements are already in place
+
+//         for (i = 0; i < n - 1; i++)
+//             for (j = 0; j < n - i - 1; j++)
+//                 if (nums[j] > nums[j + 1])
+//                     swap(nums[j], nums[j + 1]);
+
+//         return;
+//     }
+// };
+
 class Solution
 {
 public:
     void sortColors(vector<int> &nums)
     {
-        // sort(nums.begin(), nums.end());
-
-        // bubble sort
-        int i, j;
         int n = nums.size();
-
-        // Last i elements are already in place
-
-        for (i = 0; i < n - 1; i++)
-        {
-            for (j = 0; j < n - i - 1; j++)
-            {
-                if (nums[j] > nums[j + 1])
+        int temp = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if (nums[i] > nums[j])
                 {
-                    swap(nums[j], nums[j + 1]);
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
                 }
-            }
-        }
-
-        return;
     }
 };
