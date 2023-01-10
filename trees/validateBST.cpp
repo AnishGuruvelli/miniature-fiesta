e746d5af436c49952a875668853f39e924c60110
AnishGuruvelli <guruvellianish@gmail.com>
2023-01-10 15:25:57 +0530

    10/01

--- trees/validateBST.cpp
+++ trees/validateBST.cpp
@@ -14,30 +14,16 @@ class Solution
 public:
     bool helper(TreeNode *root, long low, long high)
     {
-        // Empty trees are valid BSTs.
-        if (root == NULL)
-        {
+        if (!root)
             return true;
-        }
 
-        // Condition of Binary Search Tree
-        // left side node values of the root should be less value than the root and right side should be greater
-        // If that is true check recursivley for the root child's
+        if (root->val > low && root->val < high)
+            return helper(root->left, low, root->val) && helper(root->right, root->val, high);
 
-        if ((root->val < high) && (root->val > low))
-        {
-            return (helper(root->left, low, root->val) && helper(root->right, root->val, high));
-        }
-        else
-        {
-            // if this root node violates the min/max constraint
-            return false;
-        }
+        return false;
     }
-
     bool isValidBST(TreeNode *root)
     {
-        bool ans = helper(root, LONG_MIN, LONG_MAX);
-        return ans;
+        return helper(root, LONG_MIN, LONG_MAX);
     }
 };
