122e904350fe3fe7428bf09b3598b24d1652da2b
AnishGuruvelli <guruvellianish@gmail.com>
2023-01-25 11:23:59 +0530

    25/01

--- binarySearch/firstBadVersion.cpp
+++ binarySearch/firstBadVersion.cpp
@@ -6,17 +6,19 @@ class Solution
 public:
     int firstBadVersion(int n)
     {
-        int start = 0, end = n;
-        cout << end - start << end;
-        while (end - start > 1)
+        // for(int i=1;i<=n;i++)
+        //     if(isBadVersion(i)) return i;
+
+        int start = 1, end = n, mid;
+        while (start < end)
         {
-            int mid = start + (end - start) / 2;
-            /** mid = (start+end) / 2; **/
+            mid = start + (end - start) / 2;
+
             if (isBadVersion(mid))
                 end = mid;
             else
-                start = mid;
+                start = mid + 1;
         }
-        return end;
+        return start;
     }
 };
