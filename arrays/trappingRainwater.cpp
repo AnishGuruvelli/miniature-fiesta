6eeed914a31d1e90177a9d816a2ad9fc619105a9
AnishGuruvelli <guruvellianish@gmail.com>
2023-01-30 00:20:52 +0530

    30/01

--- arrays/trappingRainwater.cpp
+++ arrays/trappingRainwater.cpp
@@ -7,60 +7,53 @@
 class Solution
 {
 public:
-    /*
-    int trap(vector<int> &height)
-    {
-        if (height.empty())
-            return 0;
+    // int trap(vector<int> &height)
+    // {
+    //     if (height.empty())
+    //         return 0;
 
-        int ans = 0;
+    //     int ans = 0;
 
-        for (int i = 0; i < height.size(); i++)
-        {
-            int left_max = 0;
-            int right_max = 0;
-            for (int j = i; j >= 0; j--)
-            {
-                left_max = max(left_max, height[j]);
-            }
-            for (int k = i; k <= height.size(); k++)
-            {
-                right_max = max(right_max, height[k]);
-            }
-            ans += min(left_max, right_max) - height[i];
-        }
-        return ans;
-    }
-    */
+    //     for (int i = 0; i < height.size(); i++)
+    //     {
+    //         int left_max = 0;
+    //         int right_max = 0;
+    //         for (int j = i; j >= 0; j--)
+    //             left_max = max(left_max, height[j]);
+    //         for (int k = i; k <= height.size(); k++)
+    //             right_max = max(right_max, height[k]);
 
-    /*
-     int trap(vector<int> &height)
-     {
-         if (height.empty())
-             return 0;
+    //         ans += min(left_max, right_max) - height[i];
+    //     }
+    //     return ans;
+    // }
 
-         int ans = 0;
+    // int trap(vector<int> &height)
+    // {
+    //     if (height.empty())
+    //         return 0;
 
-         int size = height.size();
+    //     int ans = 0;
 
-         vector<int> left_max(size), right_max(size);
-         left_max[0] = height[0];
-         for (int i = 1; i < size; i++)
-         {
-             left_max[i] = max(height[i], left_max[i - 1]);
-         }
-         right_max[size - 1] = height[size - 1];
-         for (int i = size - 2; i >= 0; i--)
-         {
-             right_max[i] = max(height[i], right_max[i + 1]);
-         }
-         for (int i = 1; i < size - 1; i++)
-         {
-             ans += min(left_max[i], right_max[i]) - height[i];
-         }
-         return ans;
-     }
-     */
+    //     int size = height.size();
+
+    //     vector<int> left_max(size), right_max(size);
+    //     left_max[0] = height[0];
+    //     for (int i = 1; i < size; i++)
+    //     {
+    //         left_max[i] = max(height[i], left_max[i - 1]);
+    //     }
+    //     right_max[size - 1] = height[size - 1];
+    //     for (int i = size - 2; i >= 0; i--)
+    //     {
+    //         right_max[i] = max(height[i], right_max[i + 1]);
+    //     }
+    //     for (int i = 1; i < size - 1; i++)
+    //     {
+    //         ans += min(left_max[i], right_max[i]) - height[i];
+    //     }
+    //     return ans;
+    // }
 
     // 2POINTER APPROACH: BEST ONE
 
