26715110088a1a924f357d6918ff3d82a51134ef
AnishGuruvelli <guruvellianish@gmail.com>
2023-02-18 17:49:23 +0530

    18/02

--- dp/jumpGame.cpp
+++ dp/jumpGame.cpp
@@ -1,23 +1,48 @@
+// Time Limit Exceeded
+// Last Executed Input
+// 75 / 170 testcases passed
+
+// class Solution
+// {
+// public:
+//     bool helper(vector<int> &nums, int index, int n)
+//     {
+//         if (index == n - 1)
+//             return true;
+
+//         int reachable = index + nums[index];
+
+//         for (int i = index + 1; i <= reachable; i++)
+//             if (helper(nums, i, n))
+//                 return true;
+
+//         return false;
+//     }
+//     bool canJump(vector<int> &nums)
+//     {
+//         int n = nums.size();
+//         return helper(nums, 0, n);
+//     }
+// };
+
 class Solution
 {
 public:
-    bool helper(vector<int> &nums, int index, int n)
-    {
-        if (index == n - 1)
-            return true;
-
-        int reachable = index + nums[index];
-
-        for (int i = index + 1; i <= reachable; i++)
-            if (helper(nums, i, n))
-                return true;
-
-        return false;
-    }
     bool canJump(vector<int> &nums)
     {
         int n = nums.size();
-        return helper(nums, 0, n);
+        vector<int> dp(n, 0);
+        dp[0] = true;
+
+        for (int i = 1; i < n; i++)
+            for (int j = i - 1; j >= 0; j--)
+                if (dp[j] && j + nums[j] >= i)
+                {
+                    dp[i] = true;
+                    break;
+                }
+
+        return dp[n - 1];
     }
 };
 
